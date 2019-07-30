d1d836cd586f9766cde23cfc1338487dab81de86
Joe Hutchinson <48301850+JoeLearns2Code@users.noreply.github.com>
2019-07-29 22:36:24 -0700

    Style updates on cards.  Starting on js logic.

--- src/components/CharacterCard/index.js
+++ src/components/CharacterCard/index.js
@@ -1,24 +1,12 @@
 import React from "react";
 import "./style.css";
 
-function CharacterCard(props) {
-  return (
-    <div className="card">
-      <div className="img-container">
-        <img alt={props.name} src={props.image} />
-      </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Name:</strong> {props.name}
-          </li>
-        </ul>
-      </div>
-      <span onClick={() => props.removeFriend(props.id)} className="remove">
-        𝘅
-      </span>
+const CharacterCard = props => (
+  <div className="card" onClick={() => props.clickCount(props.id)}>
+    <div className="img-container">
+      <img alt={props.name} src={props.image} />
     </div>
-  );
-}
+  </div>
+);
 
 export default CharacterCard;
