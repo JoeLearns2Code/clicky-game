e3379c911a120a91f9f26ded2511ba6375e987f0
Joe Hutchinson <48301850+JoeLearns2Code@users.noreply.github.com>
2019-07-29 21:58:47 -0700

    Created JSON, saved images, setup initial card template.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import CharacterCard from "./components/CharacterCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import characters from "./characters.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    characters
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Title>Final Fantasy VII</Title>
+        {this.state.characters.map(character => (
+        <CharacterCard
+          id={character.id}
+          key={character.id}
+          name={character.name}
+          image={character.image}
+        />
+        ))}
+      </Wrapper>
+    );
+  }
 }
+
 
 export default App;
