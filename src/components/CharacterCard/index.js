de1f9cc898a888fbb7e3a45447186b05fcb39c8a
Joe Hutchinson <48301850+JoeLearns2Code@users.noreply.github.com>
2019-07-29 21:21:22 -0700

    Initial file upload.

---
+++ src/components/CharacterCard/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./style.css";
+
+function CharacterCard(props) {
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Name:</strong> {props.name}
+          </li>
+        </ul>
+      </div>
+      <span onClick={() => props.removeFriend(props.id)} className="remove">
+        𝘅
+      </span>
+    </div>
+  );
+}
+
+export default CharacterCard;
