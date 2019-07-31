97af1bf2f16600b70b791dd8c2fc97ab9183bddc
Joe Hutchinson <48301850+JoeLearns2Code@users.noreply.github.com>
2019-07-30 23:33:18 -0700

    Added README file.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,15 @@
 import React from "react";
 import "./style.css";
 
-// function Header(props) {
-//   return <h1 className="title">{props.children}</h1>;
-  
-
-// }
 
 const Header = props => (
   <div className="header">
     <div className="title">{props.children}</div>
     <div className="scores">
-      Score: {props.score} 
+      Score: {props.score}
     </div>
     <div className="highscore">
-    Highscore: {props.highscore}
+      Highscore: {props.highscore}
     </div>
   </div>
 );
